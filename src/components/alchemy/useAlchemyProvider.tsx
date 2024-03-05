a01cb554ae55709eb5b01bdd558895e7a936b57e
Richard Oh <richard@magic.link>
2024-03-05 01:21:47 -0600

    creates alchemy hook

--- src/components/alchemy/useAlchemyProvider.tsx
+++ src/components/alchemy/useAlchemyProvider.tsx
@@ -1,81 +1,51 @@
-import {
-  getDefaultLightAccountFactoryAddress,
-  LightSmartContractAccount,
-} from "@alchemy/aa-accounts"
-import { SmartAccountSigner, WalletClientSigner } from "@alchemy/aa-core"
-import { AlchemyProvider } from "@alchemy/aa-alchemy"
-import { sepolia } from "viem/chains"
-import { createWalletClient, custom, WalletClient } from "viem"
-import { useCallback, useEffect, useMemo, useState } from "react"
-import { useMagic } from "../magic/MagicProvider"
+import { useEffect, useMemo, useCallback, useState } from 'react';
+import { useMagic } from '../magic/MagicProvider';
+import { 
+  type SmartAccountClient, 
+  type SmartAccountSigner, 
+  WalletClientSigner, 
+  sepolia 
+} from '@alchemy/aa-core';
+import { createModularAccountAlchemyClient } from '@alchemy/aa-alchemy';
+import { getAlchemyApiKey } from '@/utils/network';
 
 // Initializes the useAlchemyProvider hook for managing AlchemyProvider in a React component.
 export const useAlchemyProvider = () => {
-  const chain = sepolia
-  const lightAccountFactoryAddress = getDefaultLightAccountFactoryAddress(chain)
-  const entryPointAddress = useMemo(
-    () => "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789",
-    []
-  )
-  const { magic } = useMagic()
-  const [provider, setProvider] = useState<AlchemyProvider>(
-    new AlchemyProvider({
-      chain,
-      entryPointAddress,
-      rpcUrl: process.env.NEXT_PUBLIC_SEPOLIA_RPC!,
-    })
-  )
+  const { magic, walletClient } = useMagic();
+  const chain = sepolia;
+  const [smartClient, setSmartClient] = useState<SmartAccountClient>();
 
-  const magicClient: WalletClient | undefined = useMemo(() => {
-    if (!magic) return
-
-    return createWalletClient({
-      transport: custom(magic.rpcProvider),
-    })
-  }, [magic])
-
+  // Create a WalletClientSigner instance using the viem walletClient from MagicProvider.
   const magicSigner: SmartAccountSigner | undefined = useMemo(() => {
-    if (!magicClient) return
-    return new WalletClientSigner(magicClient as any, "magic")
-  }, [magicClient])
+    if (!walletClient) return;
+    return new WalletClientSigner(walletClient, 'magic');
+  }, [walletClient])
+    
+  // Connect the AlchemyClient to a Smart Account using the createModularAccountAlchemyClient class.
+  const connectToSmartContractAccount = useCallback(async () => {
+    if (!magicSigner || !magic) return;
 
+    // This is where Magic is associated as the owner of the smart contract account.
+    const client = await createModularAccountAlchemyClient({
+      apiKey: getAlchemyApiKey(),
+      chain,
+      signer: magicSigner,
+      gasManagerConfig: {
+        policyId: process.env.NEXT_PUBLIC_ALCHEMY_GAS_POLICY_ID as string,
+      }
+    });
+    
+    setSmartClient(client);
+  }, [chain, magic, magicSigner])
+    
   useEffect(() => {
     if (magic?.user.isLoggedIn) {
       connectToSmartContractAccount()
-    } else {
-      disconnectFromSmartContractAccount()
     }
-  }, [magic?.user.isLoggedIn])
+  }, [magic?.user.isLoggedIn, connectToSmartContractAccount])
 
-  // Connects the AlchemyProvider to a Smart Account using the LightSmartContractAccount class.
-  // Sets the owner as the Magic account wallet
-  const connectToSmartContractAccount = useCallback(() => {
-    if (!magicSigner) return
-
-    // This is where Magic is associated as the owner of the smart contract account
-    const connectedProvider = provider.connect((provider) => {
-      return new LightSmartContractAccount({
-        rpcClient: provider,
-        owner: magicSigner,
-        chain,
-        entryPointAddress,
-        factoryAddress: lightAccountFactoryAddress,
-      })
-    })
-
-    setProvider(connectedProvider)
-    return connectedProvider
-  }, [entryPointAddress, provider])
-
-  // Disconnects the AlchemyProvider from the current account.
-  const disconnectFromSmartContractAccount = useCallback(() => {
-    const disconnectedProvider = provider.disconnect()
-    setProvider(disconnectedProvider)
-    return disconnectedProvider
-  }, [provider])
-
-  // Returns the AlchemyProvider for use in components.
+  // Returns the SmartClient for use in components.
   return {
-    provider,
+    smartClient,
   }
 }
